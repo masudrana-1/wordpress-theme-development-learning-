fc98d31c4871e0e6936306536f69967bd7525e13
masudrana-1 <masudrana58855@gmail.com>
2024-02-15 20:26:11 +0600

    added head and footer and body class

--- index.php
+++ index.php
@@ -13,10 +13,13 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ) ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <!-- head and footer must be added  -->
     <?php wp_head(  ); ?>
 </head>
-<body>
+
+<!-- added body class for wordpress class  -->
+<body <?php body_class(  ); ?>>
     
     <?php wp_footer(  ); ?>
 </body>
