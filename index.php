b0235c3bdb4267ef7ca6ad3224c0c3a7566f48fd
masudrana-1 <masudrana58855@gmail.com>
2024-02-15 20:07:28 +0600

    added language attribuite

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php language_attributes(  ); ?>" class="no-js">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
