d54aa9f30270fbc27c090c443b30d3d2aea6e964
masudrana-1 <masudrana58855@gmail.com>
2024-02-16 19:32:35 +0600

    enqueue style , first unique name and link style

--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@
 
     // Theme CSS and jQuery File calling 
     function masud_css_js_file_calling(){
-
-    };
-    add_action( 'wp_enqueue_script','masud_css_js_file_calling' )
+        wp_enqueue_style( 'masud-style', get_stylesheet_uri(  ) ); //unique style name , style uri
+    }
+    add_action( 'wp_enqueue_scripts','masud_css_js_file_calling' );  //first working purpose and 2nd function name
 
 
