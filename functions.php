cfefe058306e723b7516545f2c71c06866d85288
masudrana-1 <masudrana58855@gmail.com>
2024-02-16 19:22:35 +0600

    create a css and js calling function and add action

--- functions.php
+++ functions.php
@@ -6,4 +6,10 @@
     // Theme title 
     add_theme_support( 'title-tag' );
 
+    // Theme CSS and jQuery File calling 
+    function masud_css_js_file_calling(){
+
+    };
+    add_action( 'wp_enqueue_script','masud_css_js_file_calling' )
+
 
