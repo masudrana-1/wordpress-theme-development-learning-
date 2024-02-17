49e6d187fda924cd8ff7185a549de2137988944a
masudrana-1 <masudrana58855@gmail.com>
2024-02-17 19:16:47 +0600

    added logo

--- index.php
+++ index.php
@@ -24,7 +24,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-3">
-                    
+                    <a href="">
+                        <!-- add logo  -->
+                        <img src="<?php echo get_template_directory_uri(  ); ?>/img/logo.png" alt="">
+                    </a>
                 </div>
                 <div class="col-md-3">
 
