d17daf927b9bd762a516b058a20d8f9596607b57
masudrana-1 <masudrana58855@gmail.com>
2024-02-22 19:42:53 +0600

    create foooter

--- index.php
+++ index.php
@@ -77,6 +77,17 @@
             </div>
         </div>
     </section>
+
+
+    <footer id="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </footer>
     
     <?php wp_footer(  ); ?>
 </body>
