32295d70934494e638de87201ab4da17975c4718
masudrana-1 <masudrana58855@gmail.com>
2024-02-22 19:40:50 +0600

    walker menu doesn't work properly

--- functions.php
+++ functions.php
@@ -104,3 +104,14 @@
     //! Menu register
     // jekono ekta name and text domain
     register_nav_menu( 'main_menu', __('Main_menu', 'masudRana') );
+
+
+    //! Walker menu properties
+    // function masud_nav_description($item_output, $item, $args) {
+    // if (!empty($item->description)) {
+    //         $item_output = str_replace($args->link_after . '</a>', '<span class="walker_nav">' . $item->description . '</span>' . $args->link_after . '</a>', $item_output);
+    //     }
+    // return $item_output;
+    // }
+
+    // add_filter('nav_menu_item', 'masud_nav_description', 10, 3);
