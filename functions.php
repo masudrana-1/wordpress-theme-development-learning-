df2cbf4c309d2e146d38c9b02a807668f5bab610
masudrana-1 <masudrana58855@gmail.com>
2024-02-27 19:33:32 +0600

    add default function in funtion

--- functions.php
+++ functions.php
@@ -3,8 +3,9 @@
         my theme function 
     */
 
-    // Theme title 
-    add_theme_support( 'title-tag' );
+
+    //! all Default function here 
+    include_once('inc/default.php');
 
     // Theme CSS and jQuery File calling 
     function masud_css_js_file_calling(){
