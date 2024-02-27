7f650344ae04326a3da4fed9ac331d4aada6a959
masudrana-1 <masudrana58855@gmail.com>
2024-02-27 19:25:23 +0600

    slice code and slice footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+    <footer id="footer-area">
+        <section id="copyright_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- show dynamic footer  -->
+                        <p><?php echo get_theme_mod( 'masud_copyright_section' ); ?></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </footer>
+    
+    <?php wp_footer(  ); ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -27,19 +27,8 @@
 
     <!-- footer area  -->
 
-    <footer id="footer-area">
-        <section id="copyright_area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <!-- show dynamic footer  -->
-                        <p><?php echo get_theme_mod( 'masud_copyright_section' ); ?></p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </footer>
-    
-    <?php wp_footer(  ); ?>
-</body>
-</html>
+<?php
+
+get_footer(  );
+
+?>
