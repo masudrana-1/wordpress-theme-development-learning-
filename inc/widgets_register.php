a2f71496b3cf5c97e64c9d9ba2cabc5bd44267d2
masudrana-1 <masudrana58855@gmail.com>
2024-03-09 20:55:30 +0600

    add footer widget

--- inc/widgets_register.php
+++ inc/widgets_register.php
@@ -21,6 +21,24 @@ function masud_widgets_register(){
         'before_title' => '<h2 class="title">',
         'after_title' => '</h2>',
     ));
+    register_sidebar(array(
+        'name' => __('Footer 2', 'alihossain'),
+        'id'   => 'footer-2',
+        'description' => __('Apperas in the sidebar in blog page and also other page', 'alihossain'),
+        'before_widget' => '<div class="child_sidebar">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="title">',
+        'after_title' => '</h2>',
+    ));
+    register_sidebar(array(
+        'name' => __('Footer 3', 'alihossain'),
+        'id'   => 'footer-3',
+        'description' => __('Apperas in the sidebar in blog page and also other page', 'alihossain'),
+        'before_widget' => '<div class="child_sidebar">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="title">',
+        'after_title' => '</h2>',
+    ));
 }
 
 add_action('widgets_init', 'masud_widgets_register');
