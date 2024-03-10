f9ff87271e458bc109cf177b7867b8c4da9adc89
masudrana-1 <masudrana58855@gmail.com>
2024-03-10 21:14:11 +0600

    create page file

---
+++ page.php
@@ -0,0 +1,86 @@
+
+<?php
+
+
+    // The template for displaying pages 
+
+
+    get_header(  );
+
+?>
+
+
+
+<!-- main section  -->
+
+    <section id="body_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9">
+
+                <!-- blog section  -->
+                <!-- if have any post  then show post  -->
+
+                    <?php
+
+                        if(have_posts(  )) : 
+                            while (have_posts(  )) : the_post(  );
+                    ?>
+
+                <!-- blog section  -->
+                <div class="blog_area">
+
+                <!-- the_permalink() dile link clickable hoy  -->
+
+                        <div class="post_thumb">
+                            <a href="<?php the_permalink(); ?>"><?php echo the_post_thumbnail('post-thumbnails'); ?></a>
+                        </div>
+
+                        <!-- for perfect blog page  -->
+                        <div class="post_details">
+                            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                            <?php the_excerpt(); ?>
+                        </div>
+
+                        <?php the_content(  ); ?>
+                </div>
+
+                <!-- if have not any post  then show   -->
+                    <?php
+                        endwhile;
+
+                        else : 
+                            _e( 'No post found' );
+
+                        endif;
+                    ?>
+
+                <!-- pagination  -->
+
+                <div id="page_nav">
+                    <?php if ('masud_pagenav') {masud_pagenav(); } else{ ?>
+                        <?php next_posts_link(); ?>
+                        <?php previous_posts_link(); ?>
+                    <?php } ?>
+                </div>
+
+
+                    
+                </div>
+                    <div class="col-md-3">
+                        <?php
+                            get_sidebar(  );
+                        ?>
+                    </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- footer area  -->
+
+<?php
+
+get_footer(  );
+
+?>
