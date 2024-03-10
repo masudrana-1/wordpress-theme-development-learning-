1ac1ca8646d6f188ed68fd8a9641ebd5538457f1
masudrana-1 <masudrana58855@gmail.com>
2024-03-10 21:34:24 +0600

    create single post page

---
+++ single.php
@@ -0,0 +1,43 @@
+
+<?php
+
+
+    // The template for displaying pages 
+
+
+    get_header(  );
+
+?>
+
+
+
+<!-- main section  -->
+
+    <section id="body_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9">
+
+                    <!-- this area for template  part -->
+
+                    <?php get_template_part( 'template_part/post_setup' )  ?>
+
+                    
+                </div>
+                <div class="col-md-3">
+                    <?php
+                        get_sidebar(  );
+                    ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- footer area  -->
+
+<?php
+
+get_footer(  );
+
+?>
